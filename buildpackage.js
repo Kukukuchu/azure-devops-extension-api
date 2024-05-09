1ec4411b1ba925105ad9a52c5b27c38e56a3d849
Damir Petrac <git@stada.boo>
2024-05-09 21:37:12 +0200

    Output ESM

--- buildpackage.js
+++ buildpackage.js
@@ -2,79 +2,43 @@
  * This is solely a build script, intended to prep the azure-devops-extension-api npm package for publishing.
  */
 
-const { execSync } = require("child_process");
-const fs = require("fs");
-const glob = require("glob");
-const path = require("path");
-const copy = require("recursive-copy");
-const shell = require("shelljs");
-const UglifyES = require("uglify-es");
+import { execSync } from "child_process";
+import glob from "glob";
+import path from "path";
+import shell from "shelljs";
 
-(async function() {
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-    // Clean bin directory
-    console.log("# Cleaning bin. Running shelljs rm -rf ./bin");
-    shell.rm("-rf", "./bin");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    // Compile typescript
-    console.log("# Compiling TypeScript. Executing `node_modules\\.bin\\tsc -p ./tsconfig.json`.");
-    try {
-        execSync("node_modules\\.bin\\tsc -p ./tsconfig.json", {
-            stdio: [0, 1, 2],
-            shell: true,
-            cwd: __dirname,
-        });
-    } catch (error) {
-        console.log("ERROR: Failed to build TypeScript.");
-        process.exit(1);
-    }
+// Clean bin directory
+console.log("# Cleaning lib. Running shelljs rm -rf ./lib");
+shell.rm("-rf", "./bin");
 
-    // Copy ts files to bin
-    console.log("# Copy declare files to bin.");
-    try {
-        await copy(path.join(__dirname, "src"), path.join(__dirname, "bin"), {
-            filter: f => {
-                return f.endsWith(".d.ts");
-            },
-        });
-    } catch (e) {
-        console.log("Copy failed. " + error);
-    }
-
-    // Uglify JavaScript
-    console.log("# Minifying JS using the UglifyES API, replacing un-minified files.");
-    let count = 0;
-
-    const files = await new Promise((resolve, reject) => {
-        glob("./bin/**/*.js", (err, files) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(files);
-            }
-        });
+// Compile typescript
+console.log("# Compiling TypeScript. Executing `node_modules/.bin/tsc -p ./tsconfig.json`.");
+try {
+    execSync("node_modules/.bin/tsc -p ./tsconfig.json", {
+        stdio: [0, 1, 2],
+        shell: true,
+        cwd: __dirname,
     });
+} catch (error) {
+    console.log("ERROR: Failed to build TypeScript.", error);
+    process.exit(1);
+}
 
-    for (const file of files) {
-        if (file.includes("node_modules/")) {
-            continue;
-        }
-        fs.writeFileSync(
-            file.substr(0, file.length - 2) + "min.js",
-            UglifyES.minify(fs.readFileSync(file, "utf-8"), { compress: true, mangle: true }).code,
-            "utf-8",
-        );
-        count++;
+// Copy ts files to bin
+console.log("# Copy declare files to lib.");
+try {
+    const declarationFiles = glob.sync("src/**/*.d.ts");
+    for (const src of declarationFiles) {
+        const dest = src.replace("src/", "lib/");
+        shell.mkdir("-p", path.dirname(dest));
+        shell.cp(src, dest);
     }
-    console.log(`-- Minified ${count} files.`);
-
-    // Copy package.json, LICENSE, README.md to bin
-    console.log("# Copying package.json, LICENSE, and README.md to bin.");
-    try {
-        await copy(path.join(__dirname, "package.json"), path.join(__dirname, "bin", "package.json"));
-        await copy(path.join(__dirname, "LICENSE"), path.join(__dirname, "bin", "LICENSE"));
-        await copy(path.join(__dirname, "README.md"), path.join(__dirname, "bin", "README.md"));
-    } catch (error) {
-        console.log("ERROR: Failed to copy package.json, LICENSE, or README.md - " + error);
-    }
-})();
+} catch (e) {
+    console.error("Copy failed. ", e);
+}
